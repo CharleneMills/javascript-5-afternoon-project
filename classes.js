cb98bc8716c382d25f211d695d1f0e36dd3df21e
CharleneMills <charleneskater@gmail.com>
2020-05-11 15:45:49 -0600

    CLASSES DONE!

--- classes.js
+++ classes.js
@@ -29,7 +29,21 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor (first_name, last_name, email, age){
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+  }
+
+  makeWidget(){
+    return `${this.first_name} ${this.last_name} Widget`;
+  }
+
+}
+
+
 
 
 ////////// PROBLEM 2 //////////
@@ -47,7 +61,30 @@
   Call your new class Manager
 */
 
-//Code Here
+class Manager extends Employee {
+  constructor (first_name, last_name, email, age){
+    super(first_name, last_name, email, age);
+    this.reports = [];
+    
+  }
+
+  hire(employee){
+    this.reports.push(employee);
+  }
+
+  fire(index){
+    this.reports.splice(index, 1);
+  }
+
+}
+
+const adam = new Employee('Adam', 'Trammell', 'adma@me.com', '34');
+const charlene = new Manager('Charlene', 'Mills', 'char@me.com', '34')
+
+charlene.hire('adam');
+
+
+
 
 
 ////////// PROBLEM 3 //////////
@@ -71,7 +108,38 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager extends Manager{
+  constructor(first_name, last_name, email, age, reports, title, bonus){
+    super(first_name, last_name, email, age, reports);
+    this.title = 'Not a manager';
+    this.bonus = 0;
+  }
+
+  hire(){
+    super.hire();
+    const numReports = this.reports.length;
+    if(numReports >= 1 && numReports <= 3){
+      return this.title = 'Barely Manager';
+    } else if (numReports >= 4 && numReports <= 10){
+      return this.title =  'Mostly Manager';
+    } else if (numReports >= 11 && numReports <= 50){
+      return this.title = 'Manager';
+    } else if (numReports >= 51 && numReports <= 100 ){
+      return this.title = 'Manager Plus' ;
+    } else if (numReports >= 101){
+      return this.title = 'Bestest Manager' ;
+    }
+
+  }
+
+
+  fire(){
+    super.fire();
+    this.bonus += 100;
+  }
+
+
+}
 
 
 
